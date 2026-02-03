320ec819b09da1046f738213ea083492aa1ef30e
slashtube <tommy.ferpet6@gmail.com>
2026-02-03 12:44:39 +0100

    feat(index.js): Added Logger feat(addnote.js): Added json schema validation using JOI

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 import express from 'express';
 import path from 'path';
+import { createLogger, transports, format } from 'winston';
 import { addnote } from './routes/addnote.js'
 import { init_db } from './database/db.js';
+
+// Inits Logger
+// TODO: add logging to file
+const logger = createLogger({
+	format: format.combine(
+		format.json(),
+		format.splat(),
+		format.simple(), // remove after debugging
+		format.errors({ stack: true }),
+		format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' })
+	),
+	transports: [
+		new transports.Console()
+	]
+})
 
 // Inits database
 await init_db();
@@ -19,5 +35,7 @@ app.use(express.static(path.join(import.meta.dirname, "frontend")));
 
 
 app.listen(port, () => {
-	console.log(`listening on http://localhost:${port}`);
+	logger.log('info', `listening on http://localhost:${port}`);
 })
+
+export { logger };
--- routes/addnote.js
+++ routes/addnote.js
@@ -3,21 +3,37 @@
 import express from 'express';
 import { pool } from '../database/db.js'
 import { format } from 'fecha';
+import { logger } from '../index.js';
+import Joi from 'joi';
+
+
+const schema = Joi.object({
+	name: Joi.string()
+		.alphanum()
+		.min(1)
+		.max(32)
+		.required(),
+	groupID: Joi.number()
+		.integer()
+		.positive(),
+	content: Joi.string()
+		.max(256),
+});
 
 const addnote = express.Router();
 
 addnote.post("/addnote", async (req, res) => {
+
+	const { error } = schema.validate(req.body);
+	if (error) {
+		logger.log('error', error);
+		res.sendStatus(400);
+		return;
+	}
+
 	const { name } = req.body;
 	const { groupID } = req.body;
 	const { content } = req.body;
-
-	// TODO: sanity check on the body fields
-
-	// Checks if there's a name
-	if (!name) {
-		res.sendStatus(400).send("Bad Request");
-		return;
-	}
 
 	// Gets current date
 	const creation = format(Date.now(), 'YYYY-MM-DD HH:mm:ss');
@@ -30,16 +46,14 @@ addnote.post("/addnote", async (req, res) => {
 
 		console.log(creation);
 
-		const result = await connection.query("INSERT INTO Notes (Name, GroupID, Content, Creation, LastModify) VALUES (?, ?, ?, ?, ?)",
+		await connection.query("INSERT INTO Notes (Name, GroupID, Content, Creation, LastModify) VALUES (?, ?, ?, ?, ?)",
 			[name, groupID, content, creation, creation]
 		);
 
-		// Debugging
-		console.log(result);
 		res.sendStatus(200);
 
 	} catch (err) {
-		console.log(err);
+		logger.log('error', err);
 		res.sendStatus(500);
 	} finally {
 		if (connection) connection.release();
